d9bb96966f3d668a5ac538a126ae0f8c487293d0
Daniel <tackied@merrimack.edu>
2025-05-02 22:32:22 -0400

    Added description for project objectives

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,53 +1,64 @@
-import { Image } from 'expo-image';
-import { StyleSheet } from 'react-native';
-
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { Pressable, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Minute To Win It!🥇 </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Addition</ThemedText>
-        <ThemedText>
-          You will have 60 seconds to test your addition skills. Think you can solve 60 addition questions in 60 seconds?
-          <ThemedText type="defaultSemiBold">
-          </ThemedText>{' '}
+  return (   
 
+    <ThemedView style={styles.container}>
+    <ThemedView style={styles.titleContainer}>
+      <ThemedText type="title">Minute To Win It!🥇 </ThemedText>
+      <ThemedView style={{height:40}}/>
+    </ThemedView>
+
+    <ThemedView style={styles.stepContainer}>
+      <ThemedText>
+This program is targeted towards kids in elementary school to help them improve on their mathematic skills. This is part B of my Week 7 Project. Enjoy.
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Subtraction</ThemedText>
-        <ThemedText>
-          {`Think Fast! What's 11 - 6? Do you think you can beat our high score? Click here to play more.`}
+        </ThemedView>
+
+    <Pressable onPress={() => console.log('Addition')}>
+    <ThemedView style={styles.stepContainer}>
+      <ThemedText type="subtitle">Addition</ThemedText>
+      <ThemedText>
+        You will have 60 seconds to test your addition skills. Think you can solve 60 addition questions in 60 seconds? Click Here
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Multiplication</ThemedText>
-        <ThemedText>
-          {`This one is only for the brave. 60 speed questions in 1 minute. Dare to play? `}
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        </ThemedView>
+        </Pressable>
+
+  <Pressable onPress={() => console.log('Subtraction')}>
+    <ThemedView style={styles.stepContainer}>
+      <ThemedText type="subtitle">Subtraction</ThemedText>
+      <ThemedText>
+        {`Think Fast! What's 11 - 6? Do you think you can beat our high score? Click here to play more.`}
+      </ThemedText>
+    </ThemedView>
+  </Pressable>  
+
+  <Pressable onPress={() => console.log('Multiplication')}>
+    <ThemedView style={styles.stepContainer}>
+      <ThemedText type="subtitle">Multiplication</ThemedText>
+      <ThemedText>
+        {`This one is only for the brave. 60 speed questions in 1 minute. Dare to play? Click Here`}
+      </ThemedText>
+    </ThemedView>
+   </Pressable> </ThemedView>
+
   );
-}
 
+}
 const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    padding: 30,
+    gap:16,
+    justifyContent: 'center'
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    gap: 40,
+    marginTop: 40,
 
   },
   stepContainer: {
