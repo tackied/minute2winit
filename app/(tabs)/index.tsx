2514d5440e37585ffdf2c724607faf8cef159dee
Daniel <tackied@merrimack.edu>
2025-05-03 03:06:11 -0400

    Added Colors and fixed format up.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,72 +4,98 @@ import { Pressable, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (   
-
     <ThemedView style={styles.container}>
-    <ThemedView style={styles.titleContainer}>
-      <ThemedText type="title">Minute To Win It!🥇 </ThemedText>
+    <ThemedView style={[styles.titleContainer, {backgroundColor : 'transparant'}]}>
+      <ThemedText style={{color: '#ff7c1e'}} type="title">Minute To Win It!🥇 </ThemedText>
       <ThemedView style={{height:40}}/>
     </ThemedView>
 
-    <ThemedView style={styles.stepContainer}>
-      <ThemedText>
+    <ThemedView style={[styles.titleContainer, {backgroundColor : 'transparant'}, {marginBottom : 70}]}>
+      <ThemedText style={{color: '#ff9f5b'}} > 
 This program is targeted towards kids in elementary school to help them improve on their mathematic skills. This is part B of my Week 7 Project. Enjoy.
         </ThemedText>
         </ThemedView>
 
-    <Pressable onPress={() => console.log('Addition')}>
-    <ThemedView style={styles.stepContainer}>
-      <ThemedText type="subtitle">Addition</ThemedText>
-      <ThemedText>
+    <Pressable 
+    onPress={() => console.log('Addition')}
+      style ={({pressed}) => [
+        styles.stepContainer,
+        {
+          backgroundColor: pressed ? '#9d87ff' : 'transparent',
+        }
+      ]}
+    >
+      <ThemedText style={{color: '#ff7c1e'}}type="subtitle">Addition</ThemedText>
+      <ThemedText style={{color: '#ff9f5b'}} >
         You will have 60 seconds to test your addition skills. Think you can solve 60 addition questions in 60 seconds? Click Here
         </ThemedText>
-        </ThemedView>
+  
         </Pressable>
 
-  <Pressable onPress={() => console.log('Subtraction')}>
-    <ThemedView style={styles.stepContainer}>
-      <ThemedText type="subtitle">Subtraction</ThemedText>
-      <ThemedText>
-        {`Think Fast! What's 11 - 6? Do you think you can beat our high score? Click here to play more.`}
-      </ThemedText>
-    </ThemedView>
-  </Pressable>  
+        <Pressable 
+    onPress={() => console.log('Subtraction')}
+      style ={({pressed}) => [
+        styles.stepContainer,
+        {
+          backgroundColor: pressed ? '#9d87ff' : 'transparent',
+        }
+      ]}
+    >
+      <ThemedText style={{color: '#ff7c1e'}} type="subtitle">Subtraction</ThemedText>
+      <ThemedText style={{color: '#ff9f5b'}} >
+        You will have 60 seconds to test your addition skills. Think you can solve 60 addition questions in 60 seconds? Click Here
+        </ThemedText>
+  
+        </Pressable>
 
-  <Pressable onPress={() => console.log('Multiplication')}>
-    <ThemedView style={styles.stepContainer}>
-      <ThemedText type="subtitle">Multiplication</ThemedText>
-      <ThemedText>
-        {`This one is only for the brave. 60 speed questions in 1 minute. Dare to play? Click Here`}
-      </ThemedText>
-    </ThemedView>
-   </Pressable> </ThemedView>
 
-  );
+        <Pressable 
+    onPress={() => console.log('Multiplication')}
+      style ={({pressed}) => [
+        styles.stepContainer,
+        {
+          backgroundColor: pressed ? '#9d87ff' : 'transparent',
+        }
+      ]}
+    >
+      <ThemedText style={{color: '#ff7c1e'}} type="subtitle">Multiplication</ThemedText>
+      <ThemedText style={{color: '#ff9f5b'}} >
+        You will have 60 seconds to test your addition skills. Think you can solve 60 addition questions in 60 seconds? Click Here
+        </ThemedText>
+  
+        </Pressable>
+        </ThemedView>
 
-}
+);
+  
+  }
+
+
 const styles = StyleSheet.create({
+  background: {
+    flex:1,
+    resizeMode: 'cover',
+  },
   container: {
     flex:1,
     padding: 30,
     gap:16,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#313131'
   },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 40,
-    marginTop: 40,
+    gap: 20,
+
 
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    padding: 10,
+    borderRadius: 8,
+    borderColor: '#ff7c1e',
+    borderWidth: 2,
   },
 });
